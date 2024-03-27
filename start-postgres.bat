7482521a3d46b2edaee6e53ec23eb867109008d3
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-27 12:37:35 +0100

    Add scripts for starting Postgres container

---
+++ start-postgres.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Starting the Postgres container...
+docker run --name quotes-hw-postgres -p 5432:5432 -e POSTGRES_PASSWORD=aN9e94poEi -d postgres
+echo Postgres is up and running on port 5432.
+pause
