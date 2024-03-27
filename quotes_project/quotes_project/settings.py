79362b1b19d12be91da68a6faad62c7df8ea4a3d
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-27 12:43:48 +0100

    Connect PostgreSQL to the Django project

--- quotes_project/quotes_project/settings.py
+++ quotes_project/quotes_project/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'quotes_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'aN9e94poEi',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
