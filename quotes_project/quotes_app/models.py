f476ba1950e5866f710c5ce3b88becc141b5491d
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-27 12:59:00 +0100

    Create models for authors and citations

--- quotes_project/quotes_app/models.py
+++ quotes_project/quotes_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Author(models.Model):
+    fullname = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    birth_location = models.CharField(max_length=150)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=False, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Quote(models.Model):
+    quote = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
